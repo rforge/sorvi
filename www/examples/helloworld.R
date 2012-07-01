4b329f4472b73e0a8bda1a1ae4d2d9e117e15984
lml <lml@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2012-07-01 14:34:01 +0000

    helloworld simplified
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/sorvi@156 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- www/examples/helloworld.R
+++ www/examples/helloworld.R
@@ -1,54 +1,20 @@
-# (C) 2011-2012 Louhos <louhos.github.com> All rights reserved.
-# License: FreeBSD, http://en.wikipedia.org/wiki/BSD_licenses
+# Copyright (C) 2012 Louhos (louhos.github.com)
+# Contact: louhos.github.com/contact.html
+# All rights reserved.
 
-# Tama esimerkki on testattu sorvi-paketin versiolla 0.1.71
-# Asennusohjeet: louhos.github.com/sorvi
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the FreeBSD License:
+# http://en.wikipedia.org/wiki/BSD_licenses
 
-# Esimerkki Suomen kuntatason vaestonkasvutilastojen (Tilastokeskus)
-# visualisoinnista Maanmittauslaitoksen karttadatalla (vuonna 2010)
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
 
-# Lataa soRvi-kirjasto
+# This script was implemented with soRvi version 0.1.72
+
+# Plot borders for municipalities (kunta) and provinces (maakunta)
 library(sorvi)
-
-###############################################
-
-# Lue Suomen kuntarajat SpatialPolygon-muodossa
-# (C) Maanmittauslaitos 2011
-# http://www.maanmittauslaitos.fi/aineistot-palvelut/digitaaliset-tuotteet/ilmaiset-aineistot/hankinta
 data(MML)
-sp <- MML[["1_milj_Shape_etrs_shape"]][["kunta1_p"]]
-
-#################################################
-
-# Lue kuntatason vaestonkasvutiedot tilastokeskuksen StatFin-tietokannasta
-# http://www.stat.fi/tup/statfin/index.html
-# PC Axis-muodossa ja muunna data.frameksi
-px <- GetPXTilastokeskus("http://pxweb2.stat.fi/database/StatFin/vrm/muutl/080_muutl_tau_203.px")
-
-
-# Poimi taulukosta halutut tiedot
-vaestonkasvu <- subset(px,
-Väestönmuutos.ja.väkiluku == "Luonnollinen väestönlisäys" &
-Vuosi == 2010)
-
-################################################
-
-# Lisaa tiedot karttaobjektiin
-sp@data$vaestonkasvu <- vaestonkasvu$dat[match(sp$Kunta.FI, vaestonkasvu$Alue)]
-# Korvaa puuttuvat arvot nollalla
-sp[["vaestonkasvu"]][is.na(sp[["vaestonkasvu"]])] <- 0
-
-################################################
-
-# Piirra kuva
-varname <- "vaestonkasvu"
-int <- max(abs(sp[[varname]]))
-q <- PlotShape(sp, varname, type = "twoway",
-main = "Väestönkasvu 2010",
-at = seq(0 - int, 0 + int, length = 11))
-
-#png("vaestonkasvu.png")
-jpeg("vaestonkasvu.jpg")
-print(q)
-dev.off()
+kunnat <- MML[["1_milj_Shape_etrs_shape"]][["kunta1_p"]]
+PlotShape(kunnat, varname = "Maakunta")
 
